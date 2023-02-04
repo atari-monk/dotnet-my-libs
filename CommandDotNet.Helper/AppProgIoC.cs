ef790ee6dcb20066ea9abd3b07f2c308a7b065e7
atari-monk <atari.monk1@gmail.com>
2023-02-04 14:38:49 +0100

    add lib

---
+++ CommandDotNet.Helper/AppProgIoC.cs
@@ -0,0 +1,13 @@
+﻿using Serilog;
+
+namespace CommandDotNet.Helper;
+
+public abstract class AppProgIoC<TContainer>
+    : AppProgTMethod
+{
+    protected AppProgIoC(
+        ILogger log) 
+            : base(log)
+    {
+    }
+}
---
+++ CommandDotNet.Helper/AppProgRunner.cs
@@ -0,0 +1,52 @@
+﻿using CommandDotNet.NameCasing;
+using CommandDotNet.DataAnnotations;
+using CommandDotNet.Repl;
+using Serilog;
+using Config.Wrapper;
+
+namespace CommandDotNet.Helper;
+
+public abstract class AppProgRunner<TContainer, TRootCommand> 
+    : AppProgConfig<TContainer>
+        where TRootCommand : class
+{
+    private readonly AppRunner appRunner;
+
+    public AppRunner AppRunner => appRunner;
+
+    protected AppProgRunner(
+        ILogger log
+        , IConfigReader config) 
+            : base(log, config)
+    {
+        appRunner = new AppRunner<TRootCommand>();
+        Log.Verbose("AppRunner created");
+    }
+
+    protected override void SetAppRunner()
+    {
+        SetDefaults();
+        if(Settings == null) 
+        {
+            appRunner.UseRepl();
+            Log.Verbose("AppRunner on default settings");
+            return;
+        }
+        if(Settings.UseRepl)
+        {
+            appRunner.UseRepl();
+            Log.Verbose("REPL mode on");
+        }
+    }
+
+    private void SetDefaults()
+    {
+        appRunner
+            .UseDefaultMiddleware()
+            .UseNameCasing(Case.LowerCase)
+            .UseDataAnnotationValidations();
+    }
+    
+    protected override int Run(string[] args) => 
+        appRunner.Run(args);
+}
