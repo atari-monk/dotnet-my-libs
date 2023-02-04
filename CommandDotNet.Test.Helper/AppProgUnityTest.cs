040cc4fd5bf2d4071d79eddbc2ec6e80d235b5d7
atari-monk <atari.monk1@gmail.com>
2023-02-04 15:27:39 +0100

    add test

---
+++ CommandDotNet.Test.Helper/AppProgUnityTest.cs
@@ -0,0 +1,29 @@
+﻿using CommandDotNet.TestTools;
+using Config.Wrapper;
+using Serilog;
+
+namespace CommandDotNet.Unity.Helper;
+
+public class AppProgUnityTest<TRootCommand>
+    : AppProgUnity<TRootCommand>
+        where TRootCommand : class
+{
+    public AppProgUnityTest(
+        ILogger log
+        , IConfigReader config) 
+            : base(log, config)
+    {
+    }
+
+    public AppRunnerResult? AppRunnerResult { get; set; }
+
+    protected override int Run(string[] args)
+    {
+        Log.Information("Run in test mode");
+        AppRunnerResult = AppRunner.RunInMem(args);
+        var text = AppRunnerResult.Console.AllText();
+        ArgumentNullException.ThrowIfNull(text);
+        Log.Information(text);
+        return AppRunnerResult.ExitCode;
+    }
+}
