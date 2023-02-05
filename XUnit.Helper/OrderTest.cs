3e5ca9b464b9231014672ad2cd66131020507e56
atari-monk <atari.monk1@gmail.com>
2023-02-05 18:42:07 +0100

    add lib

---
+++ XUnit.Helper/OrderTest.cs
@@ -0,0 +1,8 @@
+namespace XUnit.Helper;
+
+public abstract class OrderTest
+{
+    public const string OrdererAssemblyName = "XUnit.Helper";
+    public const string OrdererTypeName = "XUnit.Helper.AlphabeticalOrderer";
+    public const string DbTests = "DbTests";
+}
