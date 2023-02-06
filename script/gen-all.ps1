752cfe1b317766b4634ff299be2ff2eccdfac03b
atari-monk <atari.monk1@gmail.com>
2023-02-07 00:45:05 +0100

    upd script

--- script/gen-all.ps1
+++ script/gen-all.ps1
@@ -7,7 +7,7 @@ $libs0 = 'DIHelper', 'CommandDotNet.IoC.Unity', 'DotNetExtension'
 #generate nugets of my libs dependent on my independent libs
 $libs1 = 'Config.Wrapper', 'CLIHelper', 'ModelHelper'
 #generate nugets of my libs dependent on my dependent libs
-$libs2 = 'EFCore.Helper', 'Serilog.Wrapper', 'CommandDotNet.Helper', 'CommandDotNet.Unity.Helper', 'DataToTable', 'Better.Console.Tables.TestApp', 'CRUDCommandHelper', 'CLIReader', 'CLIWizardHelper', 'CLIFramework'
+$libs2 = 'EFCore.Helper', 'Serilog.Wrapper', 'CommandDotNet.Helper', 'CommandDotNet.Unity.Helper', 'CommandDotNet.MDI.Helper', 'DataToTable', 'Better.Console.Tables.TestApp', 'CRUDCommandHelper', 'CLIReader', 'CLIWizardHelper', 'CLIFramework'
 
 #up from script dir
 Set-Location ..
