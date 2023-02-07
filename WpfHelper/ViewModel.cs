7e4335a96492cc55231eeb0bde181e4ac747dda5
atari-monk <atari.monk1@gmail.com>
2023-02-07 17:25:24 +0100

    add lib

---
+++ WpfHelper/ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfHelper;
+
+public abstract class ViewModel
+    : INotifyPropertyChanged
+{
+	public event PropertyChangedEventHandler? PropertyChanged;
+
+	protected void OnPropertyChanged([CallerMemberName] string? name = default)
+	{
+		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	}
+}
